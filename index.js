5ebc0a3a725c55363042f640434347e2c5fbd2be
Janis Krasemann <janis.krasemann@enpit.de>
2016-12-11 23:13:46 +0100

    update streams format

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const service = require('express')();
 
-const streams = {
-  '1live': 'http://1live.akacast.akamaistream.net/7/706/119434/v1/gnl.akacast.akamaistream.net/1live',
-  wdr2: 'https://www.wdr.de/wdrlive/media/hls/wdr2-rhein-und-ruhr.m3u8'
-};
+const streams = [
+  {
+    name: '1live',
+    url: 'http://1live.akacast.akamaistream.net/7/706/119434/v1/gnl.akacast.akamaistream.net/1live'
+  },
+  {
+    name: 'wdr2',
+    url: 'https://www.wdr.de/wdrlive/media/hls/wdr2-rhein-und-ruhr.m3u8'
+  }
+];
 
 service.get('/streams', (req, res) => {
   res.status(200).json(streams);
