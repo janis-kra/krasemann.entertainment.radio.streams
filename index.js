8cc30b514bc375158555d1b5b00f1519fd56f7ae
Janis Krasemann <janis.krasemann@enpit.de>
2016-12-22 00:58:41 +0100

    update streams

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const service = require('express')();
 
 const streams = [
-  {
-    name: '1live',
-    url: 'http://1live.akacast.akamaistream.net/7/706/119434/v1/gnl.akacast.akamaistream.net/1live'
-  },
-  {
-    name: 'wdr2',
-    url: 'https://www.wdr.de/wdrlive/media/hls/wdr2-rhein-und-ruhr.m3u8'
-  }
+  { name: '1LIVE', url: 'http://www.wdr.de/wdrlive/media/einslive.m3u' },
+  { name: 'WDR2', url: 'http://www.wdr.de/wdrlive/media/wdr2.m3u' },
+  { name: 'WDR3', url: 'http://www.wdr.de/wdrlive/media/mp3/wdr3_hq.m3u' },
+  { name: 'WDR4', url: 'http://www.wdr.de/wdrlive/media/wdr4.m3u' },
+  { name: 'WDR5', url: 'http://www.wdr.de/wdrlive/media/wdr5.m3u' },
+  { name: '89.0 RTL', url: 'http://stream.89.0rtl.de/live/mp3-256/stream.89.0rtl.de/play.m3u' },
+  { name: 'Antenne Niederrhein', url: 'http://edge.live.mp3.mdn.newmedia.nacamar.net/ps-antennenr/livestream.mp3.m3u' },
+  { name: 'Big FM', url: 'http://srv04.bigstreams.de/bigfm-mp3-64.m3u' },
+  { name: 'DasDing', url: 'http://www.dradio.de/streaming/dradiowissen.m3u' },
+  { name: 'DRadio Wissen', url: 'http://www.wdr.de/wdrlive/media/wdr5.m3u' },
+  { name: 'Energy Hamburg', url: 'http://www.listenlive.eu/energyhamburg.m3u' },
+  { name: 'ffn', url: 'http://player.ffn.de/tunein_ffn.pls' },
+  { name: 'Kiss FM', url: 'http://www.listenlive.eu/kissfmde.m3u' },
+  { name: 'N-Joy', url: 'http://www.ndr.de/resources/metadaten/audio/m3u/n-joy.m3u' },
+  { name: 'Radio Bob!', url: 'http://streams.radiobob.de/bob-live/mp3-192/listenlive/play.m3u' },
+  { name: 'Radio Bob! Classic Rock', url: 'http://streams.radiobob.de/bob-classicrock/mp3-192/listenlive/play.m3u' },
+  { name: 'SPORT1.fm', url: 'http://www.listenlive.eu/sport1.m3u' }
 ];
 
 service.get('/streams', (req, res) => {
