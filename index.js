35ce34793b29153d1e1ad2f6f61d131c95a63ae7
Janis Krasemann <janis.krasemann@enpit.de>
2016-12-31 15:13:38 +0100

    update stream urls

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ const streams = [
   { name: 'DasDing', url: 'http://www.dradio.de/streaming/dradiowissen.m3u' },
   { name: 'DRadio Wissen', url: 'http://www.wdr.de/wdrlive/media/wdr5.m3u' },
   { name: 'Energy Hamburg', url: 'http://www.listenlive.eu/energyhamburg.m3u' },
-  { name: 'ffn', url: 'http://player.ffn.de/tunein_ffn.pls' },
+  { name: 'ffn', url: 'player.ffn.de/ffn.m3u' },
   { name: 'Kiss FM', url: 'http://www.listenlive.eu/kissfmde.m3u' },
   { name: 'N-Joy', url: 'http://www.ndr.de/resources/metadaten/audio/m3u/n-joy.m3u' },
-  { name: 'Radio Bob!', url: 'http://streams.radiobob.de/bob-live/mp3-192/listenlive/play.m3u' },
+  { name: 'Radio Bob!', url: 'http://streams.radiobob.de/bob-live/mp3-128/streams.radiobob.de/play.m3u' },
   { name: 'Radio Bob! Classic Rock', url: 'http://streams.radiobob.de/bob-classicrock/mp3-192/listenlive/play.m3u' },
   { name: 'SPORT1.fm', url: 'http://www.listenlive.eu/sport1.m3u' }
 ];
